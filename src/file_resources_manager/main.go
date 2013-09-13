fba63d5ca0b2f992d3cdbdfe0ef5e2be4d0f1664
Fire Meditation [acer-ao-linux] <firemeditation@gmail.com>
2013-09-13 20:45:57 +0800

    解决配置文件的目录问题

--- src/file_resources_manager/main.go
+++ src/file_resources_manager/main.go
@@ -2,10 +2,31 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 	"github.com/msbranco/goconfig"
 )
 
 func main() {
-	c, _ := goconfig.ReadConfigFile("../config/client.cfg")
+	c := getConfig("server")
 	fmt.Println(c)
+}
+
+func getConfig(sorc string) *goconfig.ConfigFile {
+	cfg_check1 := filepath.Dir(os.Args[0]) 
+    cfg_check2 := filepath.Dir(cfg_check1)
+    var cfg_file string
+    if cfg_check1 == "." && cfg_check2 == "." {
+		cfg_file = "../config/"
+	}else{
+		cfg_file = cfg_check2 + "/config/"
+	}
+	if sorc == "server"{
+		cfg_file = cfg_file + "server.cfg"
+	}else{
+		cfg_file = cfg_file + "client.cfg"
+	}
+	fmt.Println(cfg_file)
+	c, _ := goconfig.ReadConfigFile(cfg_file)
+	return c
 }
