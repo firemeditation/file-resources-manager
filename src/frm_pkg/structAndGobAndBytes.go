ec14bc25c0b6536b2ccaec3fe2ec57ec81433337
Fire Meditation [acer-ao-linux] <firemeditation@gmail.com>
2013-09-19 21:41:12 +0800

    修改了一点注释

--- src/frm_pkg/structAndGobAndBytes.go
+++ src/frm_pkg/structAndGobAndBytes.go
@@ -5,6 +5,7 @@ import (
 	"encoding/gob"
 )
 
+// StructGobBytes 将结构体数据转成Gob再转成[]Byte
 func StructGobBytes(e interface{}) []byte {
 	var gob_buff bytes.Buffer  //建立缓冲
 	gob_en := gob.NewEncoder(&gob_buff)  //gob开始编码
@@ -13,8 +14,9 @@ func StructGobBytes(e interface{}) []byte {
 	return gob_b
 }
 
+// BytesGobStruct 将[]byte转成Gob再转成结构体
 func BytesGobStruct(f_b []byte, stur interface{}) {
-	b_buf := bytes.NewBuffer(f_b)  //将SelfLoginInfo的[]byte放入bytes的buffer中
-	b_go := gob.NewDecoder(b_buf)  //将SelfLoginInfo的buffer放入gob的decoder中
-	b_go.Decode(stur)  //将gob解码放入myLogin
+	b_buf := bytes.NewBuffer(f_b)  //将[]byte放入bytes的buffer中
+	b_go := gob.NewDecoder(b_buf)  //将buffer放入gob的decoder中
+	b_go.Decode(stur)  //将gob解码放入stur
 }
