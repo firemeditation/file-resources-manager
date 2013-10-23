85ebcff2b0c052cf74b11ab2abbee2525356056b
Fire Meditation [fire] <firemeditation@gmail.com>
2013-10-23 21:10:04 +0800

    增加资源存储位置准备

--- src/frmServer/frmServer.go
+++ src/frmServer/frmServer.go
@@ -13,18 +13,20 @@ import (
 	"database/sql"
 )
 
-var serverConfig  *goconfig.ConfigFile
-var userLoginStatus UserIsLogin
-var dbConn *sql.DB
-var logInfo *log.Logger
-var errLog *log.Logger
+var serverConfig  *goconfig.ConfigFile  //配置文件
+var userLoginStatus UserIsLogin  //登录用户表
+var dbConn *sql.DB   //数据库连接
+var storage []string  //存储盘位置
+var logInfo *log.Logger  //日志
+var errLog *log.Logger  //错误日志
 
 func init() {
-	serverConfig = GetConfig("server")
-	userLoginStatus = NewUserIsLogin()
+	serverConfig = GetConfig("server")  //初始化配置文件
+	prepareStorage()  //准备存储
+	userLoginStatus = NewUserIsLogin()  //初始化用户登录表
 	runtime.GOMAXPROCS(runtime.NumCPU())
-	dbConn = connDB()
-	prepareLog()
+	dbConn = connDB()  //初始化数据库连接
+	prepareLog()  //准备日志文件
 }
 
 func main() {
@@ -32,12 +34,12 @@ func main() {
 	service = ":" + service
 	IPAdrr, err := net.ResolveTCPAddr("tcp", service)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "出错了，错误是：", err)
+		fmt.Fprintln(os.Stderr, "出错了，错误是：", err)
 		os.Exit(1)
 	}
 	listens, err := net.ListenTCP("tcp", IPAdrr)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "出错了，错误是：", err)
+		fmt.Fprintln(os.Stderr, "出错了，错误是：", err)
 		os.Exit(1)
 	}
 	for {
@@ -78,4 +80,25 @@ func prepareLog() {
 	errFile, _ := serverConfig.GetString("server","err")
 	errw, _ := os.OpenFile(errFile, os.O_WRONLY | os.O_APPEND | os.O_CREATE , 0660)
 	errLog = log.New(errw, "frm_server : ", log.Ldate | log.Ltime)
+}
+
+func prepareStorage() {
+	theS, _ := serverConfig.GetOptions("storage")
+	for _, oneS := range theS {
+		oneSt, _ := serverConfig.GetString("storage",oneS)
+		storage = append(storage,oneSt)
+	}
+	for _, oneStorage := range storage {
+		dir , err := os.Open(oneStorage)
+		defer dir.Close()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "存储位置无法打开：", oneStorage)
+			os.Exit(1)
+		}
+		dirinfo, _ := dir.Stat()
+		if dirinfo.IsDir() == false {
+			fmt.Fprintln(os.Stderr, "存储位置需要为一个路径：", oneStorage)
+			os.Exit(1)
+		}
+	}
 }
