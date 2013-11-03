b3f0f3f82f319770cf295aeccd32ad7e5f338e30
Fire Meditation [fire] <firemeditation@gmail.com>
2013-11-03 10:19:17 +0800

    修改资源上传流程，增加了OriginFileInfoStruct结构体

---
+++ src/frmPkg/originFileInfoStruct.go
@@ -0,0 +1,15 @@
+package frmPkg
+
+import (
+	"os"
+	"time"
+)
+
+// OriginFileInfoStruct 源文件信息结构体，用于客户端获取本地需上传的文件的相关信息，并由服务端获得并解析。类似os.FileInfo的结构。
+type OriginFileInfoStruct struct {
+	RelativeDir string  //相对路径
+	FileName string  //文件名(只是文件名)
+	Size int64  //文件大小
+	Mode os.FileMode  //文件权限
+	ModeTime time.Time  //文件修改日期
+}
