61990299c1986812bcbde4475526c89844472511
Fire Meditation [fire] <firemeditation@gmail.com>
2014-03-26 20:28:12 +0800

    继续管理的开发

--- src/frmServer/static/iSystemControl.js
+++ src/frmServer/static/iSystemControl.js
@@ -14,7 +14,21 @@ var iSystemControl = {
 		$("#main-box #system-control").hide();
 		$("#allwhite").hide();
 	},
+	
+	//处理body-left的isnow=true的显示
+	blIsNow : function(){
+		$("#system-control-true .body .body-left .menu-list").each(function(index){
+			if($(this).attr("isnow") == "true"){
+				$(this).children("span").css("border-bottom","dashed 1px #ff0000");
+				//$(this).children("span").addClass("is_now_true");
+			}else{
+				//$(this).children("span").addClass("is_now_false");
+				$(this).children("span").css("border","0");
+			}
+		});
+	},
 };
 
 iSystemControl.changeSystemControlHeight(); 
 $(window).resize(function(){ iSystemControl.changeSystemControlHeight(); });
+iSystemControl.blIsNow();
